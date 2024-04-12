0a1de39dee4ad4fe4d28ee2b3f301aad2e6fd202
Altonsong <129694621+Altonsong@users.noreply.github.com>
2024-04-12 12:20:58 -0400

    Add files via upload

---
+++ data-final/addons-040412-gaetan.js
@@ -0,0 +1,20 @@
+const addons = [
+  {
+    serial: "A1",
+    name: "Test Title",
+    image: "../photos/addons-040412-gaetan/a0.jpg",
+    detail: "Test Paragraph.",
+    price: "0",
+    isChecked: false,
+  },
+  {
+    serial: "A2",
+    name: "Test Title",
+    image: "../photos/addons-040412-gaetan/a0.jpg",
+    detail: "Test Paragraph.",
+    price: "0",
+    isChecked: false,
+  },
+ 
+]
+
