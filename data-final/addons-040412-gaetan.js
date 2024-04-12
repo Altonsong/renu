7fb7f748644b8bed978fd8fce291666f03f031eb
Altonsong <129694621+Altonsong@users.noreply.github.com>
2024-04-12 14:54:45 -0400

    Update addons-040412-gaetan.js

--- data-final/addons-040412-gaetan.js
+++ data-final/addons-040412-gaetan.js
@@ -1,18 +1,35 @@
 const addons = [
   {
     serial: "A1",
-    name: "Test Title",
-    image: "../photos/addons-040412-gaetan/a0.jpg",
-    detail: "Test Paragraph.",
-    price: "0",
+    name: "Vinyl Floor and Reducer Provided by Clinet",
+    image: "../photos/addons-040412-gaetan/a1.png",
+    detail: "client has responsibility delivery to Unit inside follow RENU Schedule.",
+    price: "-550",
     isChecked: false,
   },
   {
     serial: "A2",
-    name: "Test Title",
-    image: "../photos/addons-040412-gaetan/a0.jpg",
-    detail: "Test Paragraph.",
+    name: "Hallway and Foyer Protect",
+    image: "../photos/addons-040412-gaetan/a2.png",
+    detail: "Existing flooring in hallway and foyer protect by RAM board",
     price: "0",
+    isChecked: false,
+  },
+  {
+    serial: "A3",
+    name: "Demolish Wet Wall and Drywall Finish in Ensuite Bathroom",
+    image: "../photos/addons-040412-gaetan/a3.png",
+    detail: "Demolish extra wet wall and finish by drywall in Ensuite Bathroom",
+    price: "1260",
+    isChecked: false,
+  },
+
+  {
+    serial: "A4",
+    name: "Demolish Wet Wall and Drywall Finish in Main Bathroom",
+    image: "../photos/addons-040412-gaetan/a3.png",
+    detail: "Demolish extra wet wall and finish by drywall in Ensuite Bathroom",
+    price: "1260",
     isChecked: false,
   },
  
